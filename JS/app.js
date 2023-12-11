ff5bc515cd7198ef1fa87a7fa5e684ee045a88b9
sarfrajabbasi <sarfrajk96811@gmail.com>
2023-12-11 16:13:37 +0530

    sticky navbar

--- JS/app.js
+++ JS/app.js
@@ -1,13 +1,13 @@
-const navBar = document.querySelector('.navbar');
+const navBar = document.querySelector(".navbar");
 const bottomContianer = document.querySelector(".bottom-container");
 console.log(bottomContianer.offsetTop);
 console.log(navBar.offsetHeight);
-window.addEventListener('scroll',()=>{
-    // scroll point found
-    // console.log(window.scrollY);
-    if(window.scrollY > bottomContianer.offsetTop-navBar.offsetHeight - 50){
-        navBar.classList.add('active')
-    }else{
-        navBar.classList.remove('acitve')
-    }
-})
+window.addEventListener("scroll", () => {
+  // scroll point found
+  // console.log(window.scrollY);
+  if (window.scrollY > bottomContianer.offsetTop - navBar.offsetHeight - 50) {
+    navBar.classList.add("active");
+  } else {
+    navBar.classList.remove("active");
+  }
+});
